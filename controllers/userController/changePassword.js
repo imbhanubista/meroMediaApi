06c6b19fd9a9d6bd62ac49d8fd0e0ec0a718bd58
imbhanubista <imbhanubista@gmail.com>
2022-05-04 21:23:30 +0545

    worked on user password change

--- controllers/userController/changePassword.js
+++ controllers/userController/changePassword.js
@@ -1,9 +1,35 @@
  const bcrypt = require('bcrypt')
+const Users = require('../../models/users/users.models')
 
 exports.changePassword = async(req,res)=>{
     let {password, nPassword, rPassword} = req.body
+    let userId = req.loggedInData
     try{
-        let oldPassword = 
+        let oldPassword = await Users.findOne({_id:userId._id})
+        if(!await bcrypt.compare(password, oldPassword.password)){
+            res.json({
+                type:"error",
+                msg:"Old password doesn't matched!!!"
+            })
+        }
+        else if(nPassword !== rPassword){
+            res.json({
+                type:"error",
+                msg:"New password & Repeat password doesn't matched!!!"
+            })
+        }
+        else{
+            let changedPassword = await bcrypt.hash(nPassword,15)
+            await Users.updateOne({_id:userId._id},{
+                $set:{
+                    password:changedPassword
+                }
+            })
+            res.json({
+                type:"success",
+                msg:"Password has been changed"
+            })
+        }
     }
     catch(err){
         res.json({
--- routing/user.js
+++ routing/user.js
@@ -1,5 +1,6 @@
 const { purchaseMedia, listPurchase, individualPurchase } = require('../controllers/mediaController/purchase.controller')
 const { searchMedia } = require('../controllers/mediaController/searchMedia')
+const { changePassword } = require('../controllers/userController/changePassword')
 
 const router = require('express').Router()
 
@@ -14,5 +15,7 @@ router.get('/purchased', individualPurchase)
 // search data
 router.post('/search', searchMedia)
 
+// password changed
+router.post('/change_password', changePassword)
 
 module.exports = router
